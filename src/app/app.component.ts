80b1b88efe87ed775462df2a118b8a5ecdd1421c
Rustam Tazyaev <rustytazyaev@gmail.com>
2023-07-12 10:18:16 +0400

    functionality done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+interface CellClickEvent {
+  row: any;
+  headerTitle: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test-comp-lib';
+export class AppComponent implements OnInit  {
+
+   ngOnInit() {
+    console.log('OnInit')
+  }
+   headerData = [
+    { title: 'Name' },
+    { title: 'Age' },
+    { title: 'Email' },
+    { title: 'City' }
+  ];
+
+  tableData = [
+    { Name: 'John', Age: 25, Email: 'john@example.com', City: 'Samara' },
+    { Name: 'Jane', Age: 30, Email: 'jane@example.com', City: 'Samara' },
+    { Name: 'Mike', Age: 35, Email: 'mike@example.com', City: 'Samara' },
+    { Name: 'Vic', Age: 45, Email: 'vic@example.com', City: 'Samara' }
+  ];
+
+   onCellClick(event: CellClickEvent): void {
+    console.log('Clicked cell:', event.row, 'Header title:', event.headerTitle);
+  }
+
+  onColumnClick(headerTitle: string): void {
+    console.log('Clicked column:', headerTitle);
+  }
 }
---
+++ src/app/components/my-table/my-table.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+
+interface CellClickEvent {
+  row: any;
+  headerTitle: string;
+}
+
+interface Header {
+  title: string;
+}
+
+interface Data {
+  [key: string]: any;
+}
+
+@Component({
+  selector: 'lib-my-table',
+  templateUrl: './my-table.component.html',
+  styleUrls: ['./my-table.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MyTableComponent {
+  @Input() header: Header[] = [];
+  @Input() data: Data[] = [];
+  @Output() cellClick: EventEmitter<CellClickEvent> = new EventEmitter<CellClickEvent>();
+  @Output() columnClick: EventEmitter<string> = new EventEmitter<string>();
+  
+
+  onCellClick(row: Data, headerTitle: string): void {
+    this.cellClick.emit({ row, headerTitle });
+  }
+
+  onColumnClick(headerTitle: string): void {
+    this.columnClick.emit(headerTitle);
+  }
+
+  getHeaderColumns(): string[] {
+    return this.header.map(headerColumn => headerColumn.title);
+  }
+
+  getDisplayedColumns(): string[] {
+    return this.header.map(headerColumn => headerColumn.title);
+  }
+}
+
+
